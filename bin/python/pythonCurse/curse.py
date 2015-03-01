01d1931f68c73d2a4d85d89222cd66176efe7cbf
Ethan_Larkham_leo <ethanlarkham@gmail.com>
2015-03-01 17:02:35 -0500

    Forgot to add the scripts meant for the prev commit

---
+++ bin/python/pythonCurse/curse.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import curses
+from curses import wrapper
+
+stdscr = curses.initscr()
+def main(stdscr):
+        stdscr.clear()
+
+        for i in range(0, 11):
+                v = i - 10
+                stdscr.addstr(i, 0, '10 divided by {} is {}'.format(v, 10/v))
+                stdscr.refresh()
+                stdscr.getkey()
+wrapper(main)
+
