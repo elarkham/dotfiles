a75bf336882b4ee5cc6a9ec6f0e33eb29d210136
Ethan Larkham <ethanlarkham@gmail.com>
2016-04-23 15:54:53 -0400

    Added xrandr config

---
+++ etc/xrandr/lazarus.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --off --output DVI-1 --primary --mode 1920x1080 --pos 0x120 --rotate normal --output DVI-0 --mode 1600x900 --pos 1920x0 --rotate left --output HDMI-0 --off
